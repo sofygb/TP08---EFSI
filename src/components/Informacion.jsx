3f4040b137aafa16e05c83ed1ef0186c5d56c3d6
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-07 08:53:27 -0300

    cambios

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Informacion = () => {
-    return (<h1>Informacion sobre el reciclaje</h1>)
+    return (
+        <div className="reciclaje">
+            <p>Según la ONU, la gestión de los residuos tiene importantes implicaciones de salud pública, ya que es uno de los dos principales portadores y propagadores de enfermedades infecciosas. 
+                Una gestión ineficaz de residuos sólidos genera una mala impresión en inversionistas y turistas, lo que repercute en la pérdida de reputación y oportunidades de inversión. Así introducimos las siguientes prácticas:</p>
+            <ul>
+                <il>
+                    <h2>Reducir</h2></il>
+                <il>
+                    <h2>Reutilizar</h2></il>
+                <il>
+                    <h2>Reciclar</h2></il>
+                <il>
+                    <h2>Recuperar</h2></il>            
+                </ul>
+            <p>Es así que nos vamos a centrar en la práctica del reciclaje.</p>            
+            <h1>¿Qué es el reciclaje?</h1>
+            <p>El reciclaje es un proceso que consiste en hacer una selección de los residuos generados para ser tratados en plantas especializadas creando productos para otros usos. De esta manera, se evita el uso descontrolado de recursos naturales, se ahorra energía y se reducen el volumen de residuos.</p>
+            <h1>¿Qué se puede reciclar?</h1>
+            <p>En los puntos verdes hay 3 tipos de tachos de reciclaje:</p>
+            <ul>
+                <il>
+                    <h2>Verde</h2>
+                    <p>Para objetos mayormente compuestos por vidrio, como botellas y frascos.</p></il>
+                <il>
+                    <h2>Azul</h2>
+                    <p>Papel y cartón. Para diarios, revistas, papel y cajas.</p></il>
+                <il>
+                    <h2>Amarillo</h2>
+                    <p>Plástico y latas. Para botellas de gaseosas, agua mineral y productos de limpieza, entre otros.</p></il>
+            </ul>
+        </div>    
+    )
 }
 
 export default Informacion
