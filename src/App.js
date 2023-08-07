faabae1f59caf55dec6e35bcd0304867adad4a2a
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-07 08:32:35 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Home from './components/Mapa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route index element={<Mapa />}></Route>
+          <Route path='*' element={<h1>Not Found</h1>} ></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Informacion = () => {
+    return (<h1>Informacion sobre el reciclaje</h1>)
+}
+
+export default Informacion
