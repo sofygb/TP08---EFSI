2cae5b0b67378753f8a8f567e4f15c62423464f2
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-07 08:58:57 -0300

    correccion

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Mapa from './components/Mapa';
+import Informacion from './components/Informacion';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<Home />}></Route>
-          <Route index element={<Mapa />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path='/mapa' element={<Mapa />} ></Route>
+        <Route path='/informacion' element={<Informacion />} ></Route>
           <Route path='*' element={<h1>Not Found</h1>} ></Route>
         </Route>
       </Routes>
